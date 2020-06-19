9dc3ab13a0c10341018520e406177a8e787fcf87
almfisch <info@ride-the-sky.com>
2020-06-19 08:06:45 +0200

    Update Dependencies for TYPO3 V10 Compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.3.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.0-9.5.99',
+			'typo3' => '7.6.0-10.4.99',
 		),
 		'conflicts' => array(
 		),
